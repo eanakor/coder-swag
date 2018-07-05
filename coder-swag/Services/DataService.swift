7f44c0ca61e58b160b3074993e1bdba1ff210286
Macbook Pro <macbookpro@eches-macbook-pro.local>
2018-07-05 18:38:42 +0100

    completed lesson on data models

--- coder-swag/Services/DataService.swift
+++ coder-swag/Services/DataService.swift
@@ -17,7 +17,64 @@ class DataService {
         Category(title: "DIGITAL", imageName: "digital.png")
     ]
     
+    private let hats = [
+        Product(title: "Beanie", price: "£14", imageName: "hat01.png"),
+        Product(title: "Blackie", price: "£34", imageName: "hat02.png"),
+        Product(title: "Army", price: "£41", imageName: "hat03.png"),
+        Product(title: "Snapback", price: "22", imageName: "hat03.png")
+    ]
+    
+    private let hoodies = [
+        Product(title: "253 Pro Coy Olive", price: "£32", imageName: "hoodie01.png"),
+        Product(title: "253 Pro Coy Blue", price: "£22", imageName: "hoodie02.png"),
+        Product(title: "253 Pro Coy Orange", price: "£42", imageName: "hoodie03.png"),
+        Product(title: "253 Pro Coy Red", price: "£37", imageName: "hoodie04.png")
+    ]
+    
+    private let shirts = [
+        Product(title: "Black", price: "£89", imageName: "shirt01.png"),
+        Product(title: "Grey", price: "£83", imageName: "shirt02.png"),
+        Product(title: "Red", price: "£59", imageName: "shirt03.png"),
+        Product(title: "Blue", price: "£99", imageName: "shirt04.png"),
+        Product(title: "Orange", price: "£89", imageName: "shirt05.png")
+    ]
+    
+    private let digitalGoods = [Product]()
+    
     func getCategories() -> [Category] {
         return categories
     }
+    
+    func getProducts(forCategoryTitle title:String) -> [Product] {
+        switch title {
+        case "SHIRTS":
+            return getShirts()
+        case "HATS":
+            return getHats()
+        case "HOODIES":
+            return getHoodies()
+        case "DIGITAL":
+            return getDigitalGoods()
+        default:
+            return getShirts()
+        }
+    }
+    
+    func getHats() -> [Product] {
+        return hats
+    }
+    
+    func getHoodies() -> [Product] {
+        return hoodies
+    }
+    
+    func getShirts() -> [Product] {
+        return shirts
+    }
+    
+    func getDigitalGoods() -> [Product] {
+        return digitalGoods
+    }
 }
+
+
