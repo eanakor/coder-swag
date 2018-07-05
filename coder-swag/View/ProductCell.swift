7c1301f9d8eadd44d6c3b52d1b8148f84f58540a
Macbook Pro <macbookpro@eches-macbook-pro.local>
2018-07-05 17:54:46 +0100

    finished lesson 3

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Eche's Macbook  on 05/07/2018.
+//  Copyright © 2018 Eche's Macbook . All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
